3ca479353d8fee40e01f7af6a3587e4f93bc76e1
Esther Peixoto <estherpeixoto13@gmail.com>
2025-02-21 18:04:08 -0300

    feat: complete node.js learning trail

---
+++ src/routes/get-ranking-route.ts
@@ -0,0 +1,33 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { getRanking } from '../functions/get-ranking'
+
+export const getRankingRoute: FastifyPluginAsyncZod = async app => {
+  app.get(
+    '/ranking',
+    {
+      schema: {
+        operationId: 'getRanking',
+        summary: 'Get ranking',
+        description: 'Bigger text',
+        tags: ['referral'],
+        response: {
+          200: z.object({
+            ranking: z.array(
+              z.object({
+                id: z.string(),
+                name: z.string(),
+                score: z.number(),
+              })
+            ),
+          }),
+        },
+      },
+    },
+    async () => {
+      const { rankingWithScore } = await getRanking()
+
+      return { ranking: rankingWithScore }
+    }
+  )
+}
